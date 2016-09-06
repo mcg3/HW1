290cab811acfcfda1e856dddbe64aad91b791b51
mcg3 <mcg3@rice.edu>
2016-09-06 00:33:52 -0500

    Update HW1.m

--- HW1.m
+++ HW1.m
@@ -11,8 +11,22 @@ x = 3; y = 5; % integers
 %x = '3'; y= '5';
 
 %your code goes here
+a= isnumeric(x); b= isnumeric(y);
+if a==1;
+    x=x;
+else x=str2num(x);
+    
+end
+if b==1
+    y=y;
+else y=str2num(y);
+end
+c=x+y
 
 %output your answer
+c =
+
+     8
 
 %% Problem 2 - our first real biology problem. Open reading frames and nested loops.
 
@@ -23,14 +37,36 @@ x = 3; y = 5; % integers
 % you can do this without using a loop.
 
 N = 500; % define sequence length
-
+%% Create random 500bp DNA seq
+a= 'ATCG';
+b=[1,2,3,4];
+c=randi(4,1,500);
+rand_seq=a(c);
 
 
 %part 2: open reading frames (ORFs) are pieces of DNA that can be
 % transcribed and translated. they start with a start codon (ATG) and end with a
 % stop codon (TAA, TGA, or TAG). Write a piece of code that finds the longest ORF 
 % in your seqeunce rand_seq. Hint: see the function strfind.
-
+%% find longest ORF
+startcod=strfind(rand_seq, 'ATG');
+stopcod=[strfind(rand_seq,'TAA') strfind(rand_seq,'TAG') strfind(rand_seq,'TGA')];
+sort(stopcod);
+for ii=startcod(1:end)
+    for jj=stopcod(1:end)
+        if ii<jj 
+            seg=(ii:jj);
+            size1(jj)=numel(seg);
+            div=mod(size1,3);
+            if div==0
+                orf(jj)= seg;
+            end
+        end
+    end
+    allOrf = orf(jj);
+    allSizes= size1(jj);
+end
+LongestOrf= (orf(jj)== max(allSizes,[],1));
 %part 3: copy your code in parts 1 and 2 but place it inside a loop that
 % runs 10000 times. Use this repeating operation to determine the probability
 % that an sequence of length 500 has an ORF of greater than 50 b.p.
@@ -58,9 +94,15 @@ N = 500; % define sequence length
 % part1: write code to read the Cp data from this file into a vector. You can ignore the last two
 % rows with positions beginning with G and H as there were no samples here. 
 
+%% getting select data from an excel sheet
+Cp=xlsread('HW1p3.xlsx','E3:E74');
+
 % Part 2: transform this vector into an array representing the layout of
 % the plate. e.g. a 6 row, 12 column array should that data(1,1) = Cp from
 % A1, data(1,2) = Cp from A2, data(2,1) = Cp from B1 etc. 
+
+%% transform vector into matrix of 12 columns
+layout=vec2mat(cp,12);
 
 % Part 3. The 4th gene in columns 10 - 12 is known as a normalization gene.
 % That is, it's should not change between conditions and it is used to normalize 
@@ -71,6 +113,28 @@ N = 500; % define sequence length
 % the gene in the 1st condition, CpX is the value of Cp in condition and
 % CpN0 and CpNX are the same quantitites for the normalization gene.
 % Plot this data in an appropriate way. 
+
+%% Normalization of 3 genes 
+%%find the average Cp of each gene
+for ii=1:6;
+    CpGene1(ii) =mean(layout(ii,1:3));
+    CpGene2(ii)=mean(layout(ii,4:6));
+    CpGene3(ii)=mean(layout(ii,7:9));
+    CpGene4(ii)=mean(layout(ii,10:12));
+end
+%% normalization of each gene with respect to gene4
+for ii=2:6;
+    NormGene1(ii)= 2^[CpGene1(1)-CpGene1(ii)-(CpGene4(1)-CpGene4(ii))];
+    NormGene2(ii)= 2^[CpGene2(1)-CpGene2(ii)-(CpGene4(1)-CpGene4(ii))];
+    NormGene3(ii)= 2^[CpGene3(1)-CpGene3(ii)-(CpGene4(1)-CpGene4(ii))];
+end
+
+%% Plot normalized data
+figure
+plot(1:6,NormGene1,1:6,NormGene2,'--',1:6,NormGene3,'k:')
+title('Cp Normalized for 3 genes')
+xlabel('conditions')
+ylabel('Normalized Cp')
 
 %% problem 4. starting with image data:
 
@@ -84,8 +148,19 @@ N = 500; % define sequence length
 % value for that pixel in the entire z-stack. store this in the variable
 % max_img
 
+%% To get Max Intensity
+for n=1:384
+    im=sprintf('z%d.tif',n);
+    ima=imread(im);
+    max_int = max(ima,[],3);
+end
 % part 2. write a loop that reads all the images and writes them as a
 % single 3D image. hint: see the function imwrite and its option 'append' 
 %
-
+%% to write as single 3D image
+for n=1:384
+    im=sprintf('z%d.tif',n);
+    ima=imread(im);
+    imwrite(ima,'Single3Dim','WriteMode','append');
+end
 
